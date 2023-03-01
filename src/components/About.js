cc24526b1b4ebe666348009a72c27f97ed227e31
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2023-03-01 20:01:12 +0300

    edit the home components add images in the images folder

--- src/components/About.js
+++ src/components/About.js
@@ -2,18 +2,23 @@ import React from "react"
 
 function About(){
     return (
-        <div>
-          <div></div>
-          <h1>ABOUT US</h1>
-          <p>Our mission is to provide the best possible real estate services to our clients, with integrity, professionalism, and attention to detail.</p>
-          <p>With years of experience in the industry, our team is dedicated to helping our clients buy and sell properties that meet their unique needs and exceed their expectations.</p>
-          <h2>OUR CORE VALUES</h2>
-          <ul>
-            <li>Integrity: We always act in the best interests of our clients, with honesty and transparency.</li>
-            <li>Professionalism: We are committed to providing the highest level of service and expertise, while maintaining a friendly and approachable demeanor.</li>
-            <li>Attention to Detail: We believe that every detail matters, and we work diligently to ensure that all aspects of the buying, selling, or renting process are handled with care and precision.</li>
-            <li>Community: We are proud to be a part of the local community, and we strive to give back through charitable donations and volunteer work.</li>
-          </ul>
+        <div className="aboutpochi">
+          <div className="aboutcontent">
+            <div className="aboutheader">
+            <h1 className="abouth1">ABOUT US</h1>
+              <p>With years of experience in the industry, our team is dedicated to helping our clients buy and sell properties that meet their unique needs and exceed their expectations.</p>
+            </div>
+                <div>
+                  <h2>OUR CORE VALUES</h2>
+                  <ul>
+                      <li>Integrity: We always act in the best interests of our clients, with honesty and transparency.</li>
+                      <li>Professionalism: We are committed to providing the highest level of service and expertise, while maintaining a friendly and approachable demeanor.</li>
+                      <li>Attention to Detail: We believe that every detail matters, and we work diligently to ensure that all aspects of the buying, selling, or renting process are handled with care and precision.</li>
+                      <li>Community: We are proud to be a part of the local community, and we strive to give back through charitable donations and volunteer work.</li>
+                  </ul>
+                </div>
+          </div>
+          
         </div>
       );
        
