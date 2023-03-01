26873a024f0f8e35e82c6c18339be058abe086e1
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2023-03-01 12:58:05 +0300

    modified the addproperty component

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -8,33 +8,49 @@ function AddProperty(){
              </div>
             <div className="formcont">
             <form class="row g-3">
-  <div class="col-md-5">
-    <label for="inputEmail4" class="form-label">Email</label>
-    <input type="email" class="form-control" id="inputEmail4"/>
+            <div class="col-md-5">
+    <label for="inputAddress" class="form-label">Agent Name</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="Kamau Njiruu"/>
   </div>
+
   <div class="col-md-4">
-    <label for="inputPassword4" class="form-label">Password</label>
-    <input type="password" class="form-control" id="inputPassword4"/>
-  </div>
-  <div class="col-md-5">
-    <label for="inputAddress" class="form-label">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"/>
-  </div>
-  <div class="col-md-4">
-    <label for="inputAddress2" class="form-label">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
+    <label for="inputAddress2" class="form-label">Agent Number</label>
+    <input type="number" class="form-control" id="inputAddress2" placeholder="0734*******"/>
   </div>
   <div class="col-md-5">
-    <label for="inputCity" class="form-label">City</label>
-    <input type="text" class="form-control" id="inputCity"/>
+    <label for="inputAddress2" class="form-label">City</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Banana town"/>
   </div>
   <div class="col-md-4">
-    <label for="inputState" class="form-label">State</label>
+    <label for="inputAddress2" class="form-label">Home Address</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="kinoo, z-corner"/>
+  </div>
+  <div class="col-md-5">
+    <label for="inputAddress" class="form-label">Price</label>
+    <input type="number" class="form-control" id="inputAddress" placeholder="$ 1000"/>
+  </div>
+  <div class="col-md-4">
+    <label for="inputState" class="form-label">Property type</label>
     <select id="inputState" class="form-select">
       <option selected>Choose...</option>
-      <option>...</option>
+      <option>Apartment</option>
+      <option>Townhouses</option>
+      <option>Condos</option>
+      <option>Mansions</option>
+      <option>Cottage</option>
     </select>
   </div>
+  <div class="col-md-5">
+    <label for="inputState" class="form-label">Insuarance type</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>Homeowners insurance</option>
+      <option>Condo insurance</option>
+      <option>Landlord insurance</option>
+      
+    </select>
+  </div>
+  
   
   <div class="col-12">
   </div>
