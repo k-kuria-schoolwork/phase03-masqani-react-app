8b12565db1293ba61f160d9936382643064b2369
sophia <sophiendalo@gmail.com>
2023-03-06 11:40:28 +0300

    1.Update Properties

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,42 +1,37 @@
-import React from "react";
-import {useEffect,useState} from "react"
+import React, { useState, useEffect } from "react";
+
 function PropertyList() {
   const [houses, setHouses] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:9292/')
+    fetch(" http://localhost:3000/data")
       .then(response => response.json())
-      .then(houses => setHouses(houses));
+      .then(data => setHouses(data))
+      .catch(error => console.error(error));
   }, []);
 
   const deleteHouse = id => {
-    fetch(`http://localhost:9292/property/${id}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json'
-      },
+    fetch(` http://localhost:3000/data${id}`, {
+      method: "DELETE"
     })
       .then(response => {
         if (response.ok) {
           setHouses(houses => houses.filter(house => house.id !== id));
+        } else {
+          console.log("Failed to delete house");
         }
       })
-      .catch(error => {
-        console.error(error);
-      });
+      .catch(error => console.error(error));
   };
 
   const editHouse = houseId => {
-    // Find the house object with the matching ID
     const house = houses.find(house => house.id === houseId);
 
-    // Prompt the user to enter new values for each field
-    const newTitle = prompt('Enter a new title for the house:', house.name);
-    const newDescription = prompt('Enter a new description for the house:', house.description);
-    const newImageUrl = prompt('Enter a new image URL for the house:', house.image_url);
-    const newPrice = prompt('Enter a new price for the house:', house.price);
+    const newTitle = prompt("Enter a new title for the house:", house.name);
+    const newDescription = prompt("Enter a new description for the house:", house.description);
+    const newImageUrl = prompt("Enter a new image URL for the house:", house.image_url);
+    const newPrice = prompt("Enter a new price for the house:", house.price);
 
-    // Create the updated house object
     const updatedHouse = {
       ...house,
       name: newTitle || house.name,
@@ -45,44 +40,33 @@ function PropertyList() {
       price: newPrice || house.price,
     };
 
-    // Send a PUT request to the backend API with the updated house object
-    fetch(`http://localhost:9292/property/update/${houseId}`, {
+    const updatedHouses = houses.map(house => house.id === houseId ? updatedHouse : house);
+    setHouses(updatedHouses);
+
+    // Send a PUT request to the backend API with the updated house
+    fetch(`http://localhost:3000/data/${houseId}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(updatedHouse)
     })
-      .then(response => {
-        if (response.ok) {
-          setHouses(houses => {
-            const updatedHouses = [...houses];
-            const index = updatedHouses.findIndex(house => house.id === houseId);
-            updatedHouses[index] = updatedHouse;
-            return updatedHouses;
-          });
-        } else {
-          console.log('Failed to update house object');
-        }
-      })
-      .catch(error => {
-        console.error(error);
-      });
+    .catch(error => console.error(error));
   };
 
   return (
-    <div className="card-row " width="700" height="500" style={{ display: 'flex', borderRadius: '40px', flexWrap: 'wrap' }}>
+    <div className="card-row" style={{ display: "flex", flexWrap: "wrap", borderRadius: "40px" }}>
       {houses.map(house => (
-        <div className="col-4" style={{ marginTop: '30px', borderRadius: '40px' }}>
-          <div className="card" style={{ width: '35rem' }} key={house.id}>
-            <img width="700" height="500" className="card-img-top" src={house.image_url} alt="House" />
+        <div className="col-4" style={{ marginTop: "30px", borderRadius: "40px" }} key={house.id}>
+          <div className="card" style={{ width: "35rem" }}>
+            <img className="card-img-top" src={house.image_url} alt="House" />
             <div className="card-body">
               <h5 className="card-title">{house.name}</h5>
               <p>Price: {house.price}</p>
               <p>Address: {house.address}</p>
               <p>{house.description}</p>
-              <button className="bg-danger" type="button" class="btn btn-danger" style={{ marginRight: '5px' }} onClick={() => deleteHouse(house.id)}>DELETE</button>
-              <button type="button" class="btn btn-secondary" style={{ marginRight: '30px' }} onClick={() => editHouse(house.id)}>Edit</button>
+              <button className="btn btn-danger" style={{ marginRight: "5px" }} onClick={() => deleteHouse(house.id)}>Delete</button>
+              <button className="btn btn-secondary" style={{ marginRight: "30px" }} onClick={() => editHouse(house.id)}>Edit</button>
             </div>
           </div>
         </div>
